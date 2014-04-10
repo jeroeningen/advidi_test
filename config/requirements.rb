bbaa5da2fac2068bc931af9e9f1e8a83b81a6b36
Jeroen van Ingen <allertz@stiptomedia.nl>
2014-04-10 23:31:35 +0200

    Bugfix in fog credentials

--- config/requirements.rb
+++ config/requirements.rb
@@ -31,7 +31,7 @@ if settings.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_I'],      # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],      # required
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], # required
     }
     config.fog_directory  = 'public/uploads'                   # required
