cea38759d2952858b6a0690d0231e7419578a448
Jeroen van Ingen <allertz@stiptomedia.nl>
2014-04-10 22:28:11 +0200

    Debug seedfile for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ Campaign.create :id => 1, :name => "Test campaign", :random_ratio => 100, :banne
 # The banners are seeded seperate from the campaign to overcome the error 'Errno::EMFILE: Too many open files - file -b --mime-type'
 # To overcome the error that the campaign can't be found in the function 'banner_ids_from redis', the banners must be added AFTER the campaign is added
 images = Dir.entries("#{Dir.pwd}/db/fixtures/images/").select {|f| !File.directory? f}
+raise images.inspect
 400.times do |i|
   Banner.create :id => i, :campaign_id => 1, :weight => (i % 2) + 1, :image => File.open("#{Dir.pwd}/db/fixtures/images/#{images[i]}")
 end
